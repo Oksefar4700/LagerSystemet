6c1a9070ee2dff590cbb9a5fb798761a1a2ba3d4
Danitheboi <dani43i6@edu.zealand.dk>
2023-05-30 23:01:39 +0200

    ...

--- test1/src/main/java/com/example/demo/repository/TransportCompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.TransportCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportCompanyRepository extends JpaRepository<TransportCompany, Integer> {
-}
-
